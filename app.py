b573b3e8f8dcbc167955655b826e128e2fb623c0
nazihkalo <nazihkalo@gmail.com>
2020-04-23 13:13:42 -0500

    adding session & cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -7,7 +7,7 @@ app.secret_key = 'randomkey'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes = session.keys())
 
 @app.route('/your-url', methods = ['GET', 'POST'])
 def your_url():
@@ -26,11 +26,30 @@ def your_url():
         else:
             f= request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('/Users/nazihkalo/Documents/GitHub/url-shortener/' + full_name)
+            f.save('/Users/nazihkalo/Documents/GitHub/url-shortener/static/user_files/' + full_name)
+            urls[request.form['code']] = {'file': full_name}
 
-        urls[request.form['code']] = {'url': request.form['url']}
+            
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
+            session[request.form['code']] = True
         return render_template('your-url.html', code = request.form['code'])
     else:
         return redirect(url_for('home'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                else:
+                    file_url = url_for('static', filename = 'user_files/' + urls[code]['file'])
+                    return redirect(file_url)
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
